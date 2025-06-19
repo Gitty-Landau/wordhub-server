5b8d7f4ba83224072cbc5691cf7b089368d70de6
Gitty Landau <glandau@cyberconvoy.com>
2025-06-19 08:42:07 -0400

    Update pnpm lock file

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
-import app from "./app";
+import app from './app';
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log(`Express server running on port ${PORT}`);
-});
+if (process.env.NODE_ENV !== 'production') {
+  app.listen(PORT, () => {
+    console.log(`Express server running on port ${PORT}`);
+  });
+}
 
 export default app;
